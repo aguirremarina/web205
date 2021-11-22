81e1b05ce8d8ad0e2c2e39d57218cde50b234f24
aguirremarina <94421292+aguirremarina@users.noreply.github.com>
2021-11-21 21:08:56 -0800

    W2A2
    array unique function remove duplicates

---
+++ web205/W2A2/index.php
@@ -0,0 +1,14 @@
+<?php
+
+   $input = array(2,1,1,2,2,3,4,5,5);
+   //$input = array("Marina",'Marina 2');  
+
+   echo "<br>ARRAY...:";
+
+   print_r($input);
+
+   echo "ARRAY UNIQUE:";
+
+   print_r(array_unique($input)); 
+
+?>
